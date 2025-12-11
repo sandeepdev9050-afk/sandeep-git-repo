1d0cd84df280dcaf69eec983304a9d1f47cf003f
unknown <Sandeepdev9050@gmail.com>
2025-12-11 16:04:56 +0530

    onergadd

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg-1" {
   name     = "myrg-1"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "rg-2" {
+  name     = "sandeep-rg"
+  location = "West Europe"
+}
